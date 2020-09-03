d79e52858d0e79a0d4f58a1cd132ec5d9e06f94b
Roman Sery <roman.sery@novelution.com>
2020-09-03 18:50:25 -0400

    Updates

---
+++ precache-manifest.ab44bf5d3670b4a10fcdc11d683b73c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b36c25ed4fd9c4e7c86358191c1a71df",
+    "url": "/react-monopoly-board/index.html"
+  },
+  {
+    "revision": "c6ab0be26d2ad938a8b9",
+    "url": "/react-monopoly-board/static/css/main.9eb9d71f.chunk.css"
+  },
+  {
+    "revision": "9449069b6ce200a4d97d",
+    "url": "/react-monopoly-board/static/js/2.b15989b3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-monopoly-board/static/js/2.b15989b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ab0be26d2ad938a8b9",
+    "url": "/react-monopoly-board/static/js/main.c63eedd9.chunk.js"
+  },
+  {
+    "revision": "861ae311390275f8a49c",
+    "url": "/react-monopoly-board/static/js/runtime-main.52c43c5d.js"
+  }
+]);
